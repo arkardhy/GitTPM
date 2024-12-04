9a7c219b9be210dc54ce07e7f49b8eb7870f7687
arkardhy <arkardhyid@gmail.com>
2024-12-04 21:26:37 +0700

    erroragain

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -7,6 +8,11 @@ export default defineConfig({
     port: 5173,
     host: true,
     strictPort: true
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
   },
   optimizeDeps: {
     exclude: ['lucide-react']
